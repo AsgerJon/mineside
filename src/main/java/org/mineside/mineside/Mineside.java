2da2d96cefb099da44c68ce786513341fb443782
AsgerJon <asgerjon2@gmail.com>
2024-07-14 02:59:03 +0200

    Initial commit

--- src/main/java/org/mineside/mineside/Mineside.java
+++ src/main/java/org/mineside/mineside/Mineside.java
@@ -1,2 +1,11 @@
-public class Mineside {
+package org.mineside.mineside;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Mineside implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        // Initialization logic here.
+        System.out.println("Mineside has been initialized.");
+    }
 }
--- src/main/java/org/mineside/mineside/client/MinesideClient.java
+++ src/main/java/org/mineside/mineside/client/MinesideClient.java
@@ -1,2 +1,15 @@
-public class MinesideClient {
+package org.mineside.mineside.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+
+@Environment(EnvType.CLIENT)
+public class MinesideClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // Client-specific initialization logic here.
+        System.out.println("Mineside client has been initialized.");
+    }
 }
